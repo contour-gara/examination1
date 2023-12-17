25c6572e865bfa5adaee9614f5c931a97d64a79b
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-18 01:17:02 +0900

    first commit

---
+++ src/test/java/org/contourgara/examination1gara/Examination1ApplicationTests.java
@@ -0,0 +1,10 @@
+package org.contourgara.examination1gara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examination1ApplicationTests {
+  @Test
+  void contextLoads() {}
+}
