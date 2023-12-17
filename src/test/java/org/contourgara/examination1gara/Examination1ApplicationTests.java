85fbbd99df7fe84de1b439c5f2fa943b7f37487f
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-18 01:20:30 +0900

    DI テスト追加

--- src/test/java/org/contourgara/examination1gara/Examination1ApplicationTests.java
+++ src/test/java/org/contourgara/examination1gara/Examination1ApplicationTests.java
@@ -1,10 +1,18 @@
 package org.contourgara.examination1gara;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Examination1ApplicationTests {
+  @Autowired
+  TempComponent tempComponent;
+
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+    assertThat(tempComponent).isNotNull();
+  }
 }
