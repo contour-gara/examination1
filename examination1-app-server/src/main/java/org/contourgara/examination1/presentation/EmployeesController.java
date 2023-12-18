0399ce95951c5b0642d6b1e466cfea8b1623e296
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-19 01:28:28 +0900

    rootへアクセスする実装完了

--- examination1-app-server/src/main/java/org/contourgara/examination1/presentation/EmployeesController.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/presentation/EmployeesController.java
@@ -1,4 +1,22 @@
 package org.contourgara.examination1.presentation;
 
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 従業員情報のエンドポイントです。
+ */
+@RestController
+@RequestMapping("/")
 public class EmployeesController {
+  /**
+   * root URL にアクセスされた場合、ステータスコード 200 を返します。
+   */
+  @GetMapping
+  @ResponseStatus(OK)
+  public void root() {}
 }
