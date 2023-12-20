7d934aca926c25185b68b8a4e06bcc304ce5b258
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 01:45:45 +0900

    EmployeeId 実装 (グリーン)

--- examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/EmployeeId.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/EmployeeId.java
@@ -1,5 +1,7 @@
 package org.contourgara.examination1.domain.model;
 
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
 /**
  * EmployeeId クラスは従業員 ID を表すクラスです。
  *
@@ -12,6 +14,7 @@ public record EmployeeId(String value) {
    * @param value 従業員 ID。1 以上 999999999 以下の数字で、null であってはなりません。
    */
   public EmployeeId {
+    if (!isNumeric(value)) throw new IllegalArgumentException("従業員 ID は数字です。");
     if ((Integer.parseInt(value) <= 0) || (Integer.parseInt(value) >= 999999999)) {
       throw new IllegalArgumentException("従業員 ID は 1 以上 999999999 以下です。");
     }
