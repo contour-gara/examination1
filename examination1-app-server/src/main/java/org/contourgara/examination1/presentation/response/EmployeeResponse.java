057e6997ad0d063202c364ee251146c5890b3386
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 03:16:25 +0900

    EmployeesResponse 実装 (green)

--- examination1-app-server/src/main/java/org/contourgara/examination1/presentation/response/EmployeeResponse.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/presentation/response/EmployeeResponse.java
@@ -1,10 +1,31 @@
 package org.contourgara.examination1.presentation.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.contourgara.examination1.domain.model.Employee;
 
+/**
+ * 従業員を表すレスポンス用オブジェクトです。
+ *
+ * @param id 従業員 ID。
+ * @param firstName 名前。
+ * @param lastName 名字。
+ */
 public record EmployeeResponse(
-  @JsonProperty("id") String id,
-  @JsonProperty("firstName") String firstName,
-  @JsonProperty("lastName") String lastName
+    @JsonProperty("id") String id,
+    @JsonProperty("firstName") String firstName,
+    @JsonProperty("lastName") String lastName
 ) {
+  /**
+   * EmployeeResponse を初期化します。
+   *
+   * @param employee 従業員オブジェクト。
+   * @return 従業員レスポンス用オブジェクト。
+   */
+  public static EmployeeResponse of(Employee employee) {
+    return new EmployeeResponse(
+        employee.employeeId().getValue(),
+        employee.firstName(),
+        employee.lastName()
+    );
+  }
 }
