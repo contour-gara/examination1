a62afb3e25fd53292b8c6bdbae3eadc81760b0d1
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 01:24:58 +0900

    EmployeeId 実装

--- examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/EmployeeId.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/EmployeeId.java
@@ -1,4 +1,19 @@
 package org.contourgara.examination1.domain.model;
 
-public record EmployeeId() {
+/**
+ * EmployeeId クラスは従業員 ID を表すクラスです。
+ *
+ * @param value 従業員 ID。1 以上 999999999 以下の数字で、null であってはなりません。
+ */
+public record EmployeeId(String value) {
+  /**
+   * EmployeeId を初期化します。
+   *
+   * @param value 従業員 ID。1 以上 999999999 以下の数字で、null であってはなりません。
+   */
+  public EmployeeId {
+    if (Integer.parseInt(value) <= 0) {
+      throw new IllegalArgumentException("従業員 ID は 1 以上 999999999 以下です。");
+    }
+  }
 }
