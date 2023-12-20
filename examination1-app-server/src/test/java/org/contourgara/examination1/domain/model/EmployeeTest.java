0c80ba9931e1b7571fdaa4830f6a5773bddf14b7
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 02:24:16 +0900

    Employee テスト実装 (red)

--- examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeTest.java
+++ examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeTest.java
@@ -6,11 +6,21 @@ import static org.assertj.core.api.Assertions.*;
 
 class EmployeeTest {
   private static final EmployeeId EMPLOYEE_ID = new EmployeeId("1");
+  private static final String FIRST_NAME = "Taro";
+  private static final String LAST_NAME = "Yamada";
+
+  @Test
+  void 従業員IDがnullの場合例外が飛ぶ() {
+    // setup
+    assertThatCode(() -> new Employee(null, FIRST_NAME, LAST_NAME))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("従業員 ID は null であってはなりません。");
+  }
 
   @Test
   void 名前がnullの場合例外が飛ぶ() {
     // setup
-    assertThatCode(() -> new Employee(EMPLOYEE_ID, null, "Yamada"))
+    assertThatCode(() -> new Employee(EMPLOYEE_ID, null, LAST_NAME))
       .isInstanceOf(IllegalArgumentException.class)
       .hasMessage("名前は null であってはなりません。");
   }
