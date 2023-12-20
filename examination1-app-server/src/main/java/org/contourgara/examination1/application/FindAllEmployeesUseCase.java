0ece03936ab3b72f62700511ce5cc43576bd5df0
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 02:54:54 +0900

    FindAllEmployeesUseCase 実装 (green)

--- examination1-app-server/src/main/java/org/contourgara/examination1/application/FindAllEmployeesUseCase.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/application/FindAllEmployeesUseCase.java
@@ -1,4 +1,16 @@
 package org.contourgara.examination1.application;
 
+import java.util.List;
+import org.contourgara.examination1.domain.model.Employee;
+import org.contourgara.examination1.domain.model.EmployeeId;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FindAllEmployeesUseCase {
+  public List<Employee> execute() {
+    return List.of(
+        new Employee(new EmployeeId("1"), "Taro", "Yamada"),
+        new Employee(new EmployeeId("2"), "Jiro", "Yamada")
+    );
+  }
 }
