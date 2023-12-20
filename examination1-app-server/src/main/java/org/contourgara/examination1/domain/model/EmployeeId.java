177d6579ce93735146fc6933d168b3533f0d8ab6
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 01:53:05 +0900

    EmployeeId 修正 (refactoring)

--- examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/EmployeeId.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/EmployeeId.java
@@ -1,22 +1,29 @@
 package org.contourgara.examination1.domain.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
 import static org.apache.commons.lang3.StringUtils.isNumeric;
 
 /**
  * EmployeeId クラスは従業員 ID を表すクラスです。
- *
- * @param value 従業員 ID。1 以上 999999999 以下の数字で、null であってはなりません。
  */
-public record EmployeeId(String value) {
+@EqualsAndHashCode
+@Getter
+public class EmployeeId {
+  private final String value;
+
   /**
    * EmployeeId を初期化します。
    *
    * @param value 従業員 ID。1 以上 999999999 以下の数字で、null であってはなりません。
    */
-  public EmployeeId {
+  public EmployeeId(String value) {
     if (!isNumeric(value)) throw new IllegalArgumentException("従業員 ID は数字です。");
     if ((Integer.parseInt(value) <= 0) || (Integer.parseInt(value) >= 1000000000)) {
       throw new IllegalArgumentException("従業員 ID は 1 以上 999999999 以下です。");
     }
+
+    this.value = value;
   }
 }
