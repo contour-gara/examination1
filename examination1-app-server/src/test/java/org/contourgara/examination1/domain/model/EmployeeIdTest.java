b58cba83365f9aba36782f1a1df2c170e758901c
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 01:26:49 +0900

    EmployeeId テスト実装

--- examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeIdTest.java
+++ examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeIdTest.java
@@ -12,4 +12,12 @@ class EmployeeIdTest {
       .isInstanceOf(IllegalArgumentException.class)
       .hasMessage("従業員 ID は 1 以上 999999999 以下です。");
   }
+
+  @Test
+  void idが1000000000の場合例外が飛ぶ() {
+    // execute & assert
+    assertThatThrownBy(() -> new EmployeeId("1000000000"))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessage("従業員 ID は 1 以上 999999999 以下です。");
+  }
 }
