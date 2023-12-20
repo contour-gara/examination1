9c8a8e6e574371f7beb229dc8b1d98333ed915f4
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 03:06:06 +0900

    AllEmployeesResponse 実装 (コンパイルエラー解消)

--- examination1-app-server/src/main/java/org/contourgara/examination1/presentation/response/AllEmployeesResponse.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/presentation/response/AllEmployeesResponse.java
@@ -1,10 +1,14 @@
 package org.contourgara.examination1.presentation.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.contourgara.examination1.domain.model.Employee;
 
 import java.util.List;
 
 public record AllEmployeesResponse(
   @JsonProperty("employees") List<EmployeeResponse> employeeResponses
   ) {
+  public static AllEmployeesResponse of(List<Employee> employees) {
+    return new AllEmployeesResponse(List.of());
+  }
 }
