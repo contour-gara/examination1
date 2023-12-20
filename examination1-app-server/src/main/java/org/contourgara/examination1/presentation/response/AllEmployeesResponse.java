e9c084512b68346509821dd01faf23f685e4282d
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 03:24:27 +0900

    AllEmployeesResponse 実装 (green)

--- examination1-app-server/src/main/java/org/contourgara/examination1/presentation/response/AllEmployeesResponse.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/presentation/response/AllEmployeesResponse.java
@@ -1,14 +1,24 @@
 package org.contourgara.examination1.presentation.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import org.contourgara.examination1.domain.model.Employee;
 
-import java.util.List;
-
+/**
+ * 従業員のレスポンス用オブジェクトを複数格納できるオブジェクトです。
+ *
+ * @param employeeResponses 従業員のレスポンス用オブジェクトの List。
+ */
 public record AllEmployeesResponse(
-  @JsonProperty("employees") List<EmployeeResponse> employeeResponses
-  ) {
+    @JsonProperty("employees") List<EmployeeResponse> employeeResponses
+) {
+  /**
+   * AllEmployeeResponse を初期化します。
+   *
+   * @param employees 従業員オブジェクトの List
+   * @return 従業員のレスポンス用オブジェクトの List。
+   */
   public static AllEmployeesResponse of(List<Employee> employees) {
-    return new AllEmployeesResponse(List.of());
+    return new AllEmployeesResponse(employees.stream().map(EmployeeResponse::of).toList());
   }
 }
