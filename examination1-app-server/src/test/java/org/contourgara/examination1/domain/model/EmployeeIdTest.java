b7f106a81616cc6d4a95a887d4a7257e2abaa06d
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 01:34:28 +0900

    EmployeeId テスト実装

--- examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeIdTest.java
+++ examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeIdTest.java
@@ -1,6 +1,8 @@
 package org.contourgara.examination1.domain.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -19,5 +21,20 @@ class EmployeeIdTest {
     assertThatThrownBy(() -> new EmployeeId("1000000000"))
       .isInstanceOf(IllegalArgumentException.class)
       .hasMessage("従業員 ID は 1 以上 999999999 以下です。");
+  }
+
+  @ParameterizedTest
+  @CsvSource(textBlock = """
+    null
+    数字
+    abc
+    ''
+    ' '
+    """)
+  void idが数字でない場合例外が飛ぶ(String value) {
+    // execute & assert
+    assertThatThrownBy(() -> new EmployeeId(value))
+      .isInstanceOf(IllegalArgumentException.class)
+      .hasMessage("従業員 ID は数字です。");
   }
 }
