a64a1f8283f6568507cbea4041eb67ede60369df
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-21 01:50:11 +0900

    EmployeeId テスト修正 (refactoring)

--- examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeIdTest.java
+++ examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeIdTest.java
@@ -7,18 +7,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.assertj.core.api.Assertions.*;
 
 class EmployeeIdTest {
-  @Test
-  void idが0の場合例外が飛ぶ() {
-    // execute & assert
-    assertThatThrownBy(() -> new EmployeeId("0"))
-      .isInstanceOf(IllegalArgumentException.class)
-      .hasMessage("従業員 ID は 1 以上 999999999 以下です。");
-  }
-
-  @Test
-  void idが1000000000の場合例外が飛ぶ() {
-    // execute & assert
-    assertThatThrownBy(() -> new EmployeeId("1000000000"))
+  @ParameterizedTest
+  @CsvSource(textBlock = """
+    0
+    1000000000
+    """)
+  void idが無効な数字の場合例外が飛ぶ(String value) {
+    assertThatCode(() -> new EmployeeId(value))
       .isInstanceOf(IllegalArgumentException.class)
       .hasMessage("従業員 ID は 1 以上 999999999 以下です。");
   }
@@ -44,7 +39,7 @@ class EmployeeIdTest {
     """)
   void idが数字でない場合例外が飛ぶ(String value) {
     // execute & assert
-    assertThatThrownBy(() -> new EmployeeId(value))
+    assertThatCode(() -> new EmployeeId(value))
       .isInstanceOf(IllegalArgumentException.class)
       .hasMessage("従業員 ID は数字です。");
   }
