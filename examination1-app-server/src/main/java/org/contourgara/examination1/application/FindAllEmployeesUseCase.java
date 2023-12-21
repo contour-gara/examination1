89ca63ec472b88c8b258649d9c2f9a9be7e88515
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-22 02:18:54 +0900

    UseCase と Repository を結合 (red)

--- examination1-app-server/src/main/java/org/contourgara/examination1/application/FindAllEmployeesUseCase.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/application/FindAllEmployeesUseCase.java
@@ -1,24 +1,26 @@
 package org.contourgara.examination1.application;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.contourgara.examination1.domain.model.Employee;
 import org.contourgara.examination1.domain.model.EmployeeId;
+import org.contourgara.examination1.domain.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 /**
  * FindAllEmployeesUseCase は従業員の全件取得を行います。
  */
+@RequiredArgsConstructor
 @Service
 public class FindAllEmployeesUseCase {
+  private final EmployeeRepository repository;
+
   /**
    * 従業員の全件取得を実行します。
    *
    * @return 従業員モデルのリスト。
    */
   public List<Employee> execute() {
-    return List.of(
-        new Employee(new EmployeeId("1"), "Taro", "Yamada"),
-        new Employee(new EmployeeId("2"), "Jiro", "Yamada")
-    );
+    return repository.findAll();
   }
 }
