f21db29e3ad7bb0ef4651ea0a62658de632f7d20
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-22 02:22:02 +0900

    UseCase と Repository を結合 (green)

--- examination1-app-server/src/test/java/org/contourgara/examination1/application/FindAllEmployeesUseCaseTest.java
+++ examination1-app-server/src/test/java/org/contourgara/examination1/application/FindAllEmployeesUseCaseTest.java
@@ -1,21 +1,40 @@
 package org.contourgara.examination1.application;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 import java.util.List;
 import org.contourgara.examination1.domain.model.Employee;
 import org.contourgara.examination1.domain.model.EmployeeId;
+import org.contourgara.examination1.domain.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-@SpringBootTest
 class FindAllEmployeesUseCaseTest {
-  @Autowired
+  @InjectMocks
   FindAllEmployeesUseCase sut;
+
+  @Mock
+  EmployeeRepository repository;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
 
   @Test
   void 全ての従業員が取得できる場合() {
+    // setup
+    doReturn(
+        List.of(
+            new Employee(new EmployeeId("1"), "Taro", "Yamada"),
+            new Employee(new EmployeeId("2"), "Jiro", "Yamada")
+        )
+    ).when(repository).findAll();
+
     // execute
     List<Employee> actual = sut.execute();
 
