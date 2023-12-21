72a570399c6f7d29253557e40ca55084d764b91e
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-22 00:28:10 +0900

    パッケージ修正

--- examination1-app-server/src/test/java/org/contourgara/examination1/infrastructure/repository/EmployeeRepositoryImplTest.java
+++ examination1-app-server/src/test/java/org/contourgara/examination1/infrastructure/repository/EmployeeRepositoryImplTest.java
@@ -6,7 +6,6 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 import org.contourgara.examination1.domain.model.Employee;
 import org.contourgara.examination1.domain.model.EmployeeId;
-import org.contourgara.examination1.infrastructure.repository.EmployeeRepositoryImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
