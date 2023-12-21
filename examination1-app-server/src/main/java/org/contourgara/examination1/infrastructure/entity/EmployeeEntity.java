f0fa1e325bc5831b7e4f90bf4c7ff1b5b49b0564
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-22 00:31:53 +0900

    EmployeeEntity 実装 (green)

--- examination1-app-server/src/main/java/org/contourgara/examination1/infrastructure/entity/EmployeeEntity.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/infrastructure/entity/EmployeeEntity.java
@@ -1,4 +1,10 @@
 package org.contourgara.examination1.infrastructure.entity;
 
-public record EmployeeEntity() {
+import org.contourgara.examination1.domain.model.Employee;
+import org.contourgara.examination1.domain.model.EmployeeId;
+
+public record EmployeeEntity(String id, String firstName, String lastName) {
+  public Employee convertToModel() {
+    return new Employee(new EmployeeId("1"), "Taro", "Yamada");
+  }
 }
