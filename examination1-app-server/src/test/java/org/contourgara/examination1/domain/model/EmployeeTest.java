fe842794b6035ecbae8c2902ffca1499ecf4cd71
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-24 02:58:51 +0900

    ドメインクラスのガード条件追加 (refactoring)

--- examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeTest.java
+++ examination1-app-server/src/test/java/org/contourgara/examination1/domain/model/EmployeeTest.java
@@ -2,6 +2,7 @@ package org.contourgara.examination1.domain.model;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class EmployeeTest {
@@ -9,43 +10,49 @@ class EmployeeTest {
   private static final String FIRST_NAME = "Taro";
   private static final String LAST_NAME = "Yamada";
 
-  @Test
-  void 従業員IDがnullの場合例外が飛ぶ() {
-    // execute & assert
-    assertThatCode(() -> new Employee(null, FIRST_NAME, LAST_NAME))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("従業員 ID は null であってはなりません。");
+  @Nested
+  class メンバー変数がnullの場合 {
+    @Test
+    void 従業員IDがnullの場合例外が飛ぶ() {
+      // execute & assert
+      assertThatCode(() -> new Employee(null, FIRST_NAME, LAST_NAME))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("従業員 ID は null であってはなりません。");
+    }
+
+    @Test
+    void 名前がnullの場合例外が飛ぶ() {
+      // execute & assert
+      assertThatCode(() -> new Employee(EMPLOYEE_ID, null, LAST_NAME))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("名前は null であってはなりません。");
+    }
+
+    @Test
+    void 名字がnullの場合例外が飛ぶ() {
+      // execute & assert
+      assertThatCode(() -> new Employee(EMPLOYEE_ID, FIRST_NAME, null))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("名字は null であってはなりません。");
+    }
   }
 
-  @Test
-  void 名前がnullの場合例外が飛ぶ() {
-    // execute & assert
-    assertThatCode(() -> new Employee(EMPLOYEE_ID, null, LAST_NAME))
-      .isInstanceOf(IllegalArgumentException.class)
-      .hasMessage("名前は null であってはなりません。");
-  }
+  @Nested
+  class _100文字以上の場合 {
+    @Test
+    void 名前が100文字以上の場合例外が飛ぶ() {
+      // execute & assert
+      assertThatCode(() -> new Employee(EMPLOYEE_ID, "a".repeat(101), LAST_NAME))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("名前は 100 文字以内です。");
+    }
 
-  @Test
-  void 名字がnullの場合例外が飛ぶ() {
-    // execute & assert
-    assertThatCode(() -> new Employee(EMPLOYEE_ID, FIRST_NAME, null))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("名字は null であってはなりません。");
-  }
-
-  @Test
-  void 名前が100文字以上の場合例外が飛ぶ() {
-    // execute & assert
-    assertThatCode(() -> new Employee(EMPLOYEE_ID, "a".repeat(101), LAST_NAME))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("名前は 100 文字以内です。");
-  }
-
-  @Test
-  void 名字が100文字以上の場合例外が飛ぶ() {
-    // execute & assert
-    assertThatCode(() -> new Employee(EMPLOYEE_ID, FIRST_NAME, "a".repeat(101)))
-        .isInstanceOf(IllegalArgumentException.class)
-        .hasMessage("名字は 100 文字以内です。");
+    @Test
+    void 名字が100文字以上の場合例外が飛ぶ() {
+      // execute & assert
+      assertThatCode(() -> new Employee(EMPLOYEE_ID, FIRST_NAME, "a".repeat(101)))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("名字は 100 文字以内です。");
+    }
   }
 }
