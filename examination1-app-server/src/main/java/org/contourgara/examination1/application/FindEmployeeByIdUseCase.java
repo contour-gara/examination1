49f14ff89f3e8c74244995112152d63235f7511d
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-24 02:09:20 +0900

    ID 検索テスト実装 (green)

--- examination1-app-server/src/main/java/org/contourgara/examination1/application/FindEmployeeByIdUseCase.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/application/FindEmployeeByIdUseCase.java
@@ -1,4 +1,17 @@
+
 package org.contourgara.examination1.application;
 
+import org.contourgara.examination1.application.exception.NotFoundEmployeeException;
+import org.contourgara.examination1.domain.model.Employee;
+import org.contourgara.examination1.domain.model.EmployeeId;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class FindEmployeeByIdUseCase {
+  public Employee execute(String id) {
+    Optional<Employee> tmp = Optional.of(new Employee(new EmployeeId("1"), "Taro", "Yamada"));
+    return tmp.orElseThrow(() -> new NotFoundEmployeeException(id));
+  }
 }
