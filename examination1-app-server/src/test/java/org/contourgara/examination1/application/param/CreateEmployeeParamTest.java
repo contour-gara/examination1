6fdcf7711afb3745da76f775e983aa60da35dfe1
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-24 20:31:16 +0900

    新規登録 実装 (green)

---
+++ examination1-app-server/src/test/java/org/contourgara/examination1/application/param/CreateEmployeeParamTest.java
@@ -0,0 +1,23 @@
+package org.contourgara.examination1.application.param;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.contourgara.examination1.domain.model.Employee;
+import org.contourgara.examination1.domain.model.EmployeeId;
+import org.junit.jupiter.api.Test;
+
+class CreateEmployeeParamTest {
+  @Test
+  void モデルに変換できる() {
+    // setup
+    CreateEmployeeParam sut = new CreateEmployeeParam("Hanako", "Shirato");
+
+    // execute
+    Employee actual = sut.convertToModel("3");
+
+    // assert
+    Employee expected = new Employee(new EmployeeId("3"), "Hanako", "Shirato");
+
+    assertThat(actual).isEqualTo(expected);
+  }
+}
