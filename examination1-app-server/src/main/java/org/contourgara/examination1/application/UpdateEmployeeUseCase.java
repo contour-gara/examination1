3778dfca2ca9c8ca4f133abfd94178d213289f57
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-28 12:12:51 +0900

    従業員更新 実装 (green)

--- examination1-app-server/src/main/java/org/contourgara/examination1/application/UpdateEmployeeUseCase.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/application/UpdateEmployeeUseCase.java
@@ -1,7 +1,19 @@
 package org.contourgara.examination1.application;
 
+import lombok.RequiredArgsConstructor;
+import org.contourgara.examination1.application.exception.NotFoundEmployeeException;
+import org.contourgara.examination1.application.param.UpdateEmployeeParam;
+import org.contourgara.examination1.domain.model.Employee;
+import org.contourgara.examination1.domain.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UpdateEmployeeUseCase {
+  private final EmployeeRepository repository;
+
+  public void execute(UpdateEmployeeParam updateEmployeeParam) {
+    Employee employee = repository.findById(updateEmployeeParam.id())
+        .orElseThrow(() -> new NotFoundEmployeeException(updateEmployeeParam.id()));
+  }
 }
