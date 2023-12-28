ad60801f88a644a995f0398c6643e07d8aaaac15
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-28 12:08:42 +0900

    従業員更新 テスト (red)

---
+++ examination1-app-server/src/test/java/org/contourgara/examination1/application/UpdateEmployeeUseCaseTest.java
@@ -0,0 +1,59 @@
+package org.contourgara.examination1.application;
+
+import org.contourgara.examination1.application.exception.NotFoundEmployeeException;
+import org.contourgara.examination1.application.param.UpdateEmployeeParam;
+import org.contourgara.examination1.domain.model.Employee;
+import org.contourgara.examination1.domain.model.EmployeeId;
+import org.contourgara.examination1.domain.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class UpdateEmployeeUseCaseTest {
+  @InjectMocks
+  UpdateEmployeeUseCase sut;
+
+  @Mock
+  EmployeeRepository repository;
+
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @Test
+  void 従業員を更新できる() {
+    // setup
+    doReturn(Optional.of(new Employee(new EmployeeId("1"), "Taro", "Yamada")))
+        .when(repository)
+        .findById("1");
+
+    UpdateEmployeeParam updateEmployeeParam = new UpdateEmployeeParam("1", null, "Yamamoto");
+
+    // execute & assert
+    assertThatCode(() -> sut.execute(updateEmployeeParam))
+        .doesNotThrowAnyException();
+  }
+
+  @Test
+  void 従業員が見つからない場合() {
+    // setup
+    doReturn(Optional.empty())
+        .when(repository)
+        .findById("1");
+
+    UpdateEmployeeParam updateEmployeeParam = new UpdateEmployeeParam("1", null, "Yamamoto");
+
+    // execute & assert
+    assertThatThrownBy(() -> sut.execute(updateEmployeeParam))
+        .isInstanceOf(NotFoundEmployeeException.class)
+        .hasMessage("id = 1");
+  }
+}
