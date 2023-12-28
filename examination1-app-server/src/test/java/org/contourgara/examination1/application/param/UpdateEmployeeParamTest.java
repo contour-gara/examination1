24c8a1802518012d2527cd22a0f783b2135e902b
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-28 12:35:29 +0900

    従業員更新 テスト (red)

---
+++ examination1-app-server/src/test/java/org/contourgara/examination1/application/param/UpdateEmployeeParamTest.java
@@ -0,0 +1,24 @@
+package org.contourgara.examination1.application.param;
+
+import org.contourgara.examination1.domain.model.Employee;
+import org.contourgara.examination1.domain.model.EmployeeId;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class UpdateEmployeeParamTest {
+  @Test
+  void モデルに変換できる() {
+    // setup
+    UpdateEmployeeParam sut = new UpdateEmployeeParam("1", null, "Yamamoto");
+    Employee employee = new Employee(new EmployeeId("1"), "Taro", "Yamada");
+
+    // execute
+    Employee actual = sut.convertToModel(employee);
+
+    // assert
+    Employee expected = new Employee(new EmployeeId("1"), "Taro", "Yamamoto");
+
+    assertThat(actual).isEqualTo(expected);
+  }
+}
