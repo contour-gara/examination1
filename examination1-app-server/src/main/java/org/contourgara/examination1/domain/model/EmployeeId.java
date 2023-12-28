05efc5b2e909210ccb3c1399820a879f176773b9
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-28 19:34:20 +0900

    ドメインモデルのガード条件見直し (green)

--- examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/EmployeeId.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/EmployeeId.java
@@ -14,9 +14,14 @@ public record EmployeeId(String value) {
    * @param value 従業員 ID。1 以上 999999999 以下の数字で、null であってはなりません。
    */
   public EmployeeId {
-    if (!isNumeric(value)) throw new IllegalArgumentException("従業員 ID は数字です。");
+    if (!isNumeric(value)) {
+      throw new IllegalArgumentException(String.format("従業員 ID は数字です。[id = %s]", value));
+    }
+
     if ((Integer.parseInt(value) <= 0) || (Integer.parseInt(value) >= 1000000000)) {
-      throw new IllegalArgumentException("従業員 ID は 1 以上 999999999 以下です。");
+      throw new IllegalArgumentException(
+          String.format("従業員 ID は 1 以上 999999999 以下です。[id = %s]", value)
+      );
     }
   }
 
