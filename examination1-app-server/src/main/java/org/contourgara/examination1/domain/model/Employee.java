fab093e11116a26ad1817c2d3456259c36ec39d2
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-28 21:50:32 +0900

    ドメインモデルのガード条件見直し (refactoring)

--- examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/Employee.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/domain/model/Employee.java
@@ -6,9 +6,9 @@ import static org.apache.commons.lang3.StringUtils.isAlpha;
 /**
  * Employee クラスは従業員を表すモデルです。
  *
- * @param employeeId 従業員 ID モデル。null であってはなりません。
- * @param firstName 名前。null であってはなりません。
- * @param lastName 名字。null であってはなりません。
+ * @param employeeId {@link EmployeeId}。null であってはなりません。
+ * @param firstName 名前。アルファベットのみで空白や null であってはなりません。
+ * @param lastName 名字。アルファベットのみで空白や null であってはなりません。
  */
 public record Employee(
     EmployeeId employeeId,
@@ -18,15 +18,35 @@ public record Employee(
   /**
    * Employee を初期化します。
    *
-   * @param employeeId 従業員 ID モデル。null であってはなりません。
-   * @param firstName 名前。null であってはなりません。
-   * @param lastName 名字。null であってはなりません。
+   * @param employeeId {@link EmployeeId}。null であってはなりません。
+   * @param firstName 名前。アルファベットのみで空白や null であってはなりません。
+   * @param lastName 名字。アルファベットのみで空白や null であってはなりません。
    */
   public Employee {
     if (isNull(employeeId)) throw new IllegalArgumentException("従業員 ID は null であってはなりません。");
-    if (!isAlpha(firstName)) throw new IllegalArgumentException(String.format("firstName が不適切です。[firstName = %s]", firstName));
-    if (!isAlpha(lastName)) throw new IllegalArgumentException(String.format("lastName が不適切です。[lastName = %s]", lastName));
-    if (firstName.length() > 100) throw new IllegalArgumentException(String.format("firstName が 100 文字を超えています。[firstName = %s]", firstName));
-    if (lastName.length() > 100) throw new IllegalArgumentException(String.format("lastName が 100 文字を超えています。[lastName = %s]", lastName));
+
+    if (!isAlpha(firstName)) {
+      throw new IllegalArgumentException(
+          String.format("firstName が不適切です。[firstName = %s]", firstName)
+      );
+    }
+
+    if (!isAlpha(lastName)) {
+      throw new IllegalArgumentException(
+          String.format("lastName が不適切です。[lastName = %s]", lastName)
+      );
+    }
+
+    if (firstName.length() > 100) {
+      throw new IllegalArgumentException(
+          String.format("firstName が 100 文字を超えています。[firstName = %s]", firstName)
+      );
+    }
+
+    if (lastName.length() > 100) {
+      throw new IllegalArgumentException(
+          String.format("lastName が 100 文字を超えています。[lastName = %s]", lastName)
+      );
+    }
   }
 }
