a543f7721c616b70fc7104fdb3c8671210fa28a3
contour-gara <65527071+contour-gara@users.noreply.github.com>
2023-12-28 13:01:40 +0900

    従業員更新 修正 (refactoring)

--- examination1-app-server/src/test/java/org/contourgara/examination1/application/param/UpdateEmployeeParamTest.java
+++ examination1-app-server/src/test/java/org/contourgara/examination1/application/param/UpdateEmployeeParamTest.java
@@ -2,23 +2,38 @@ package org.contourgara.examination1.application.param;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.stream.Stream;
 import org.contourgara.examination1.domain.model.Employee;
 import org.contourgara.examination1.domain.model.EmployeeId;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 
 class UpdateEmployeeParamTest {
-  @Test
-  void モデルに変換できる() {
+  @ParameterizedTest
+  @MethodSource("createData")
+  void モデルに変換できる(UpdateEmployeeParam sut, Employee expected) {
     // setup
-    UpdateEmployeeParam sut = new UpdateEmployeeParam("1", null, "Yamamoto");
     Employee employee = new Employee(new EmployeeId("1"), "Taro", "Yamada");
 
     // execute
     Employee actual = sut.convertToModel(employee);
 
     // assert
-    Employee expected = new Employee(new EmployeeId("1"), "Taro", "Yamamoto");
-
     assertThat(actual).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> createData() {
+    return Stream.of(
+        Arguments.of(
+            new UpdateEmployeeParam("1", null, "Yamamoto"),
+            new Employee(new EmployeeId("1"), "Taro", "Yamamoto")
+        ),
+        Arguments.of(
+            new UpdateEmployeeParam("1", "Saburo", null),
+            new Employee(new EmployeeId("1"), "Saburo", "Yamada")
+        )
+    );
   }
 }
