1e533e9c9b5f971d38c0c4ed98ed355d390240ba
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-02-25 14:53:23 +0900

    受け入れテストモジュール追加

---
+++ acceptance-test/src/test/java/org/contourgara/examination1/integration/Configuration.java
@@ -0,0 +1,14 @@
+package org.contourgara.examination1.integration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties(prefix = "environment")
+public class Configuration {
+  private String baseUri;
+}
