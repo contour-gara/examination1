59bef2c2f61b0307efb033ce0825cd046152aab7
contour-gara <65527071+contour-gara@users.noreply.github.com>
2024-02-25 14:49:50 +0900

    インデント修正

--- examination1-app-server/src/main/java/org/contourgara/examination1/common/ErrorMessageCode.java
+++ examination1-app-server/src/main/java/org/contourgara/examination1/common/ErrorMessageCode.java
@@ -12,7 +12,7 @@ public enum ErrorMessageCode {
   UNEXPECTED_ERROR("0000", "unexpected exception has occurred. [%s]"),
   VALIDATE_ERROR_IN_PRESENTATION("0002", "request validation error is occurred."),
   NOT_FOUND_EMPLOYEE("0003", "specified employee [id = %s] is not found."),
-    QUERY_EXECUTION_ERROR("0004", "query execution failed. [id = %s]"),
+  QUERY_EXECUTION_ERROR("0004", "query execution failed. [id = %s]"),
   DATA_ACCESS_ERROR("0005", "unexpected exception has occurred on Database connection.");
 
   private final String code;
